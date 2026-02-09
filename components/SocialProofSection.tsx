761642fb766742049d9b412fee36e3c02a09fead
Winove <fernando@winove.com.br>
2026-02-09 17:43:03 -0300

    Adjust testimonials background sizing

--- components/SocialProofSection.tsx
+++ components/SocialProofSection.tsx
@@ -65,7 +65,7 @@ export const SocialProofSection: React.FC = () => {
           </div>
         </div>
         <div
-          className="mt-10 h-64 md:h-80 lg:h-96 bg-no-repeat bg-center bg-cover"
+          className="mt-10 w-full min-h-[220px] sm:min-h-[280px] md:min-h-[360px] lg:min-h-[420px] bg-no-repeat bg-center bg-contain"
           style={{ backgroundImage: `url(${depoimentosBackground})` }}
         />
       </div>
